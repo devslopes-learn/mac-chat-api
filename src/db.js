13921390ed53232e41a65e6909b8e2cd5c9cb87e
Jacob Luetzow <jacob@devslopes.com>
2017-05-05 15:05:24 -0700

    deply test

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import mongodb from 'mongodb';
 import config from './config';
 
 export default callback => {
-  let db = mongoose.connect(config.mongoUrl);
+  // let db = mongoose.connect(config.mongoUrl);
+  let db = mongodb.MongoClient.connect(process.env.MONGODB_URI);
   callback(db);
 }
