17308e8eded8bc1b52bc93c65bf01cf25e9bcee9
Jacob Luetzow <jacob@devslopes.com>
2017-05-05 16:07:17 -0700

    deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   "port": 3005,
-  "mongoUrl": "mongodb://localhost:27017/slacky-chat-api",
-  // "mongoUrl": "mongodb://jacob:123456@ds133221.mlab.com:33221/heroku_l1j6w33c",
+  // "mongoUrl": "mongodb://localhost:27017/slacky-chat-api",
+  "mongoUrl": "mongodb://jacob:123456@ds133221.mlab.com:33221/heroku_l1j6w33c",
   "bodyLimit": "100kb"
 }
