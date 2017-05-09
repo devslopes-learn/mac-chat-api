37024d03b568e55f55931ce13775a0754a919d3c
Jacob Luetzow <jacob@devslopes.com>
2017-05-09 08:22:30 -0700

    date to string

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ io.on('connection', function(client) {
       //Send message to those connected in the room
       console.log('new message sent');
 
-      io.emit("messageCreated",  msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp);
+      io.emit("messageCreated",  msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp.toString());
     });
   });
 });
