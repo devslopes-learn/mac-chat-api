7a1696d38826e0f26c6fb18b14510fdb2a7c2bfb
Jacob Luetzow <jacob@devslopes.com>
2017-05-11 15:17:41 -0700

    removed string cast on date

--- src/model/message.js
+++ src/model/message.js
@@ -7,7 +7,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const messageSchema = new Schema({
   messageBody: String, default: "",
-  timeStamp: {type: String, default: Date.now.toString()},
+  timeStamp: {type: Date, default: Date.now},
   userId: {type: ObjectId, ref: 'User'},
   channelId: {type: ObjectId, ref: 'Channel'},
   userName: String, default: "",
