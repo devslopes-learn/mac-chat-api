b14eeb8fd3840220836d7673cea0f362d7514625
Jacob Luetzow <jacob@devslopes.com>
2017-05-11 13:53:00 -0700

    removed unneeded code

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,6 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/slacky-chat-api",
   "port": process.env.PORT,
   "mongoUrl": process.env.MONGODB_URI,
   "bodyLimit": "100kb"
