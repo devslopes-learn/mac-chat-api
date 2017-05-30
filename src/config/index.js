919da73f2787ae225b97f9ae0e68cd204e4ff74a
jonnyb <jon.burgoyne1@gmail.com>
2017-05-29 19:02:21 -0700

    comma

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": "mongodb://jonnyb:123456@ds155191.mlab.com:55191/chattychatchat"
+  "mongoUrl": "mongodb://jonnyb:123456@ds155191.mlab.com:55191/chattychatchat",
   "bodyLimit": "100kb"
 }
