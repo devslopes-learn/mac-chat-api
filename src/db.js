0932b47e753d57dea80c97da6b7b77b1d75b2b3b
DeeJay <dheeraj.x.nagwani.-nd@disney.com>
2020-07-03 20:39:00 +0530

    added db connection retry and docker configuration

--- src/db.js
+++ src/db.js
@@ -1,19 +1,34 @@
 import mongoose from 'mongoose';
-import mongodb from 'mongodb';
 import config from './config';
 
 export default callback => {
   let db;
+  let retryCount = 0;
+
+  let connectWithRetry = function() {
+    return mongoose.connect(config.mongoUrl, function (err, database) {
+      console.log("Trying to connect to mongodb...")
+      if (err) {
+        console.log(err);
+        console.log("Waiting for 5 seconds to retry...")
+        if (retryCount < 4) {
+          setTimeout(connectWithRetry, 5000)
+          retryCount++
+          return
+        }
+
+        console.log("Giving up after 5 attempts...")
+        proccess.exit(-1)
+      }
+
+      console.log(config.mongoUrl);
+      // Save database object from the callback for reuse.
+      db = database;
+      console.log("Database connection ready");
+      callback(db);
+    });
+  }
   // Connect to the database before starting the application server.
-  mongoose.connect(config.mongoUrl, function (err, database) {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-    console.log(config.mongoUrl);
-    // Save database object from the callback for reuse.
-    db = database;
-    console.log("Database connection ready");
-    callback(db);
-  });
+
+  connectWithRetry()
 }
